e0d5f9e79bd5266bc28a8ce377fd18518f38ebfb
Aryan Mishra <mishra2001aryan@gmail.com>
2023-06-18 19:12:31 +0530

    frog Jumps

---
+++ 3.kfrogJump.cpp
@@ -0,0 +1,20 @@
+int solve(int ind, vector<int>& height, vector<int>& dp, int k){
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    
+    int mmSteps = INT_MAX;
+        
+    for(int j=1;j<=k;j++){
+        if(ind-j>=0){
+      int jump = solve(ind-j, height, dp, k)+ abs(height[ind]- height[ind-j]);
+            mmSteps= min(jump, mmSteps);
+        }
+    }
+    return dp[ind]= mmSteps;
+    
+}
+
+int kjump(int n, vector<int>& height , int k){
+    vector<int> dp(n,-1);
+    return solve(n-1, height, dp, k);
+}
